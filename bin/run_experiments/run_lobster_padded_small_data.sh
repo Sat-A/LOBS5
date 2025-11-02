b8a9ba618f2a79a9417b0f09b7a224390d01d888
saschafrey <saschayvesfrey@gmail.com>
2025-11-02 13:18:23 +0000

    Get working for AMZN data.

--- bin/run_experiments/run_lobster_padded_small_data.sh
+++ bin/run_experiments/run_lobster_padded_small_data.sh
@@ -1,15 +1,16 @@
 python3 run_train.py --USE_WANDB=True \
                     --C_init=trunc_standard_normal --prenorm=True --batchnorm=True --bidirectional=False \
-                    --blocks=16 --bsz=32 --d_model=512 --dataset=lobster-prediction --merging=padded \
-                    --dir_name='/homes/80/kang/LOBS5/GOOG_tiny' --clip_eigs=True --activation_fn=half_glu1 \
+                    --blocks=16 --bsz=4 --d_model=512 --dataset=lobster-prediction --merging=padded \
+                    --dir_name='/home/myuser/processed_data/AMZN/2024_Dec_END' --clip_eigs=True --activation_fn=half_glu1 \
                     --dt_global=False --epochs=100 --jax_seed=42 --lr_factor=1 --n_layers=12 \
                     --opt_config=standard --p_dropout=0.0 --ssm_lr_base=0.0005 --ssm_size_base=512 \
                     --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
                     --use_book_data=True --use_simple_book=False --book_transform=True  \
                     --masking=none \
-                    --num_devices=4 --n_data_workers=8 \
+                    --num_devices=1 --n_data_workers=0 \
                     --debug_loading=False \
-                    --enable_profiler=True \
+                    --enable_profiler=False \
+                    --curtail_epochs=1 \
                     #--restore='/data1/sascha/LOBS5/checkpoints/rural-serenity-60_uxc2c11x/' #\
                     #--restore='checkpoints/eager-shadow-750_af39bb9u/'
 
