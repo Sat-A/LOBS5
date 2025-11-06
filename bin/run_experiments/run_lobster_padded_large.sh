fde1fc678d49da27295fb8adce891c2d397adebb
saschafrey <saschayvesfrey@gmail.com>
2025-11-06 17:17:03 +0000

    Train only on quarter of data per epoch (random cause shuffle+offset)

--- bin/run_experiments/run_lobster_padded_large.sh
+++ bin/run_experiments/run_lobster_padded_large.sh
@@ -1,21 +1,21 @@
 python3 run_train.py --USE_WANDB=True \
                     --C_init=trunc_standard_normal --prenorm=True --batchnorm=False --bidirectional=False \
-                    --blocks=16 --bsz=24 --d_model=1024 --dataset=lobster-prediction --merging=padded \
+                    --blocks=16 --bsz=32 --d_model=1024 --dataset=lobster-prediction --merging=padded \
                     --dir_name='/home/myuser/processed_data/AMZN/2024' --clip_eigs=True --activation_fn=half_glu1 \
                     --dt_global=False --epochs=250 --jax_seed=42 --lr_factor=1 --n_layers=12 \
                     --opt_config=standard --p_dropout=0.0 --ssm_lr_base=0.0005 --ssm_size_base=1024 \
                     --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
                     --use_book_data=True --use_simple_book=False --book_transform=True  \
                     --masking=none \
-                    --num_devices=6 --n_data_workers=6 \
+                    --num_devices=8 --n_data_workers=8 \
                     --debug_loading=False \
                     --enable_profiler=False \
                     --random_offsets_train=True \
                     --shuffle_train=True \
                     --debug_overfit=False \
-
-                    # --restore='/homes/80/kang/LOBS5/checkpoints/honest-oath-159_3kn3xbd5/' \
-                    # --restore_step=63
+                    --curtail_epochs=5135 \
+                    --restore='/home/myuser/checkpoints/eager-universe-42_dccgzqjs/' \
+                    --restore_step=1
                     #--restore='checkpoints/eager-shadow-750_af39bb9u/'
                     
 
