7bd64afecc654381779f3007e10d2122236bddab
saschafrey <saschayvesfrey@gmail.com>
2025-11-09 10:03:18 +0000

    ruby-aardvark run config

--- bin/run_experiments/run_lobster_padded_large.sh
+++ bin/run_experiments/run_lobster_padded_large.sh
@@ -2,7 +2,7 @@ python3 run_train.py --USE_WANDB=True \
                     --C_init=trunc_standard_normal --prenorm=True --batchnorm=False --bidirectional=False \
                     --blocks=16 --bsz=32 --d_model=1024 --dataset=lobster-prediction --merging=padded \
                     --dir_name='/home/myuser/processed_data/AMZN/2024' --clip_eigs=True --activation_fn=half_glu1 \
-                    --dt_global=False --epochs=20 --jax_seed=42 --lr_factor=1 --n_layers=12 \
+                    --dt_global=False --epochs=40 --jax_seed=42 --lr_factor=1 --n_layers=12 \
                     --opt_config=standard --p_dropout=0.0 --ssm_lr_base=0.0003 --ssm_size_base=1024 \
                     --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
                     --use_book_data=True --use_simple_book=False --book_transform=True  \
@@ -14,8 +14,9 @@ python3 run_train.py --USE_WANDB=True \
                     --shuffle_train=True \
                     --debug_overfit=False \
                     --curtail_epochs=5135 \
-                    --restore='/home/myuser/checkpoints/swift-spaceship-43_gc848h98' \
-                    --restore_step=0
+                    --lr_patience=5 \
+                    --restore='/home/myuser/checkpoints/deep-valley-61_w3ywmoj3' \
+                    --restore_step=12
                     #--restore='checkpoints/eager-shadow-750_af39bb9u/'
                     #5135
 
