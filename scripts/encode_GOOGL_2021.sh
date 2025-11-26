e74d4900d145038c59edd696aad8390a62f81055
isambard <kang@robots.ox.ac.uk>
2025-11-26 00:18:34 +0000

    pre encode

--- scripts/encode_GOOGL_2021.sh
+++ scripts/encode_GOOGL_2021.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 编码 GOOGL 2021
 source ~/miniforge3/etc/profile.d/conda.sh && conda activate lobs5
+export JAX_PLATFORM_NAME=cpu
 mkdir -p /lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_encoded/GOOGL/2021/
 python /lus/lfs1aip2/home/s5e/kangli.s5e/AlphaTrade/LOBS5/pre_encode_data.py \
     --input_dir=/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_preproc/GOOGL/2021/ \
